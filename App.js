a27bdfbe6c22c8f40a08c263a3f6d450ff9c22e8
Chandransh Pandey <pandeychandransh@gmail.com>
2024-04-08 11:40:25 +0530

    First code commit changes done in the main branch

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+const heading1 = React.createElement("h1", {}, "Heading 1")
+const heading2 = React.createElement("h1", {}, "Heading 2")
+
+
+const container = React.createElement("h1", {id:"container"},[heading1, heading2] )
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(container)
+// hello world
+// new world

f9c0294f490d48a93461dc7e70235c09c3491ce7
Chandransh Pandey <pandeychandransh@gmail.com>
2024-04-09 10:48:55 +0530

    Added New Feature

--- App.js
+++ App.js
@@ -10,5 +10,8 @@ const container = React.createElement("h1", {id:"container"},[heading1, heading2
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(container)
+console.log("this is a new feature")
+
+
 // hello world
 // new world

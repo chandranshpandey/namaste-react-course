14e85db54b0170a7d0623a80e9c1765abbb0151f
Chandransh Pandey <chandranshpandey@hotmail.com>
2024-04-23 15:17:50 +0530

    added the most basic shimmer effect basically a note

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
-import restaurantList from "../config";
+import restaurantList from "../restaurant.js";
 import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer.js";
 
-function filterRestaurantData(textToSearch) {
-  return restaurantList.filter((ele) =>
+function filterRestaurantData(textToSearch, restList) {
+  return restList.filter((ele) =>
     ele.info.name.toLowerCase().includes(textToSearch)
   );
 }
 
 const Body = () => {
+  //just to see how many times render took place
   const [searchText, setSearchText] = useState(""); //returns an array var name and a function to update a variable
   // basically initialize a state variable with restaurant variable
-  const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [listOfAllRestaurants, setListOfAllRestaurant] = useState([]);
+
+  console.log("render");
+  console.log("All Restaurant");
+  console.log(listOfAllRestaurants);
+  console.log("Restaurant");
+  console.log(listOfRestaurant);
 
   useEffect(() => {
     async function fetchRestData() {
@@ -19,49 +28,67 @@ const Body = () => {
         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.95250&lng=75.71050&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const jsonData = await data.json();
-      console.log(jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
-      setListOfRestaurant(jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
-    }
-    fetchRestData()
+      // console.log(
+      //   jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+      // );
+      // console.log(jsonData)
 
+      //no thoughts have gone on handling the errors
+      setListOfAllRestaurant(
+        jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+      );
+      setListOfRestaurant(listOfAllRestaurants);
+    }
+    fetchRestData();
   }, []);
-  return (
-    <>
-      <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="search"
-          value={searchText}
-          onChange={(e) => {
-            console.log(e.target.value);
-            setListOfRestaurant(filterRestaurantData(e.target.value));
-            setSearchText(e.target.value);
-          }}
-        />
-        <button
-          className="search-btn"
-          onClick={() =>
-            setListOfRestaurant(
-              restaurantList.filter((ele) =>
-                ele.info.name.toLowerCase().includes(searchText)
+
+  if(listOfRestaurant.length === 0) return <h1>No Restaurant Found</h1>
+
+  if (listOfRestaurant.length === 0) {
+    return <Shimmer />;
+  } else {
+    return (
+      <>
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="search"
+            value={searchText}
+            onChange={(e) => {
+              setListOfRestaurant(
+                filterRestaurantData(e.target.value, listOfAllRestaurants)
+              );
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() =>
+              setListOfRestaurant(
+                listOfAllRestaurants.filter((ele) =>
+                  ele.info.name.toLowerCase().includes(searchText)
+                )
               )
-            )
-          }
-        >
-          Search
-        </button>
-      </div>
-      <div className="restuarant-cards">
-        {listOfRestaurant.map((restaurant) => {
-          return (
-            <RestaurantCard restaurant={restaurant} key={restaurant.info.id} />
-          );
-          // console.log(rest.info)
-        })}
-      </div>
-    </>
-  );
+            }
+          >
+            Search
+          </button>
+        </div>
+        <div className="restuarant-cards">
+          {listOfRestaurant.map((restaurant) => {
+            return (
+              <RestaurantCard
+                restaurant={restaurant}
+                key={restaurant.info.id}
+              />
+            );
+            // console.log(rest.info)
+          })}
+        </div>
+      </>
+    );
+  }
 };
 
 export default Body;
---
+++ src/config.js
@@ -0,0 +1,139 @@
+export const IMG_CDN_LINK = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+const restaurantList = [
+    {
+      info: {
+        id: "47597",
+        name: "Pizza Hut",
+        cloudinaryImageId: "2b4f62d606d1b2bfba9ba9e5386fabb7",
+        locality: "Evershine Tower",
+        areaName: "Vaishali Nagar",
+        costForTwo: "₹350 for two",
+        cuisines: ["Pizzas"],
+        avgRating: 4.1,
+        parentId: "721",
+        avgRatingString: "4.1",
+        totalRatingsString: "10K+",
+        sla: {
+          deliveryTime: 48,
+          lastMileTravel: 7.5,
+          serviceability: "SERVICEABLE",
+          slaString: "45-50 mins",
+          lastMileTravelString: "7.5 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2024-04-12 04:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "ITEMS",
+          subHeader: "AT ₹179",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+      },
+      analytics: {
+        context: "seo-data-a11548a5-1e54-4f29-aaf5-9d2b478a9d23",
+      },
+      cta: {
+        link: "https://www.swiggy.com/restaurants/pizza-hut-evershine-tower-vaishali-nagar-jaipur-47597",
+        text: "RESTAURANT_MENU",
+        type: "WEBLINK",
+      },
+      widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+    },
+    {
+      info: {
+        id: "669200",
+        name: "Chinese Wok",
+        cloudinaryImageId: "e0839ff574213e6f35b3899ebf1fc597",
+        locality: "Police Station",
+        areaName: "Vidhyadhar Nagar",
+        costForTwo: "₹250 for two",
+        cuisines: ["Chinese", "Asian", "Tibetan", "Desserts"],
+        avgRating: 4.3,
+        parentId: "61955",
+        avgRatingString: "4.3",
+        totalRatingsString: "500+",
+        sla: {
+          deliveryTime: 50,
+          lastMileTravel: 10.2,
+          serviceability: "SERVICEABLE",
+          slaString: "50-55 mins",
+          lastMileTravelString: "10.2 km",
+          iconType: "ICON_TYPE_EMPTY",
+        },
+        availability: {
+          nextCloseTime: "2024-04-12 01:00:00",
+          opened: true,
+        },
+        badges: {},
+        isOpen: true,
+        type: "F",
+        badgesV2: {
+          entityBadges: {
+            imageBased: {},
+            textBased: {},
+            textExtendedBadges: {},
+          },
+        },
+        aggregatedDiscountInfoV3: {
+          header: "ITEMS",
+          subHeader: "AT ₹179",
+        },
+        orderabilityCommunication: {
+          title: {},
+          subTitle: {},
+          message: {},
+          customIcon: {},
+        },
+        differentiatedUi: {
+          displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          differentiatedUiMediaDetails: {
+            mediaType: "ADS_MEDIA_ENUM_IMAGE",
+            lottie: {},
+            video: {},
+          },
+        },
+        reviewsSummary: {},
+        displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        restaurantOfferPresentationInfo: {},
+      },
+      analytics: {
+        context: "seo-data-a11548a5-1e54-4f29-aaf5-9d2b478a9d23",
+      },
+      cta: {
+        link: "https://www.swiggy.com/restaurants/chinese-wok-police-station-vidhyadhar-nagar-jaipur-669200",
+        text: "RESTAURANT_MENU",
+        type: "WEBLINK",
+      },
+      widgetId: "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo",
+    }
+  ];
+  export default restaurantList;
